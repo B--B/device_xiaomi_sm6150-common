e64199dc381357a2870fa65d7682f0de01c0460e
Marco Zanin <mrczn.bb@gmail.com>
2023-07-18 18:56:05 +0200

    sm6150-common: compile Kernel with clang 17.0.4
    r498229b is the latest version available in AOSP master branch.
    Let's give it a try
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,6 +49,7 @@ BOARD_SUPPORTS_SOUND_TRIGGER := true
 TARGET_BOOTLOADER_BOARD_NAME := sm6150
 TARGET_NO_BOOTLOADER := true
 
+
 # Display
 TARGET_USES_COLOR_METADATA := true
 TARGET_USES_DISPLAY_RENDER_INTENTS := true
@@ -80,6 +81,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
 TARGET_KERNEL_CONFIG := vendor/sdmsteppe-perf_defconfig
+TARGET_KERNEL_CLANG_VERSION := r498229b
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
